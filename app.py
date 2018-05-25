60b99d8fa7f10b9b0c4ebdaa9148d426087a7cbe
Ross Macduff <ross.macduff@slalom.com>
2018-05-25 14:45:21 -0400

    some cleanup

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask import render_template
-from flask_sqlalchemy import SQLAlchemy
 
 import os
 import socket
@@ -14,20 +13,14 @@ def get_env_variable(name):
         message = "Expected environment variable '{}' not set.".format(name)
         raise Exception(message)
 
-# the values of those depend on your setup
-DATABASE_HOST = get_env_variable("DATABASE_HOST")
-REDIS_HOST = get_env_variable("REDIS_HOST")
-DATABASE_PASSWORD = get_env_variable("DATABASE_PASSWORD")
-
 def connection_test(name, address, port):
     s = socket.socket()
     s.settimeout(2)
     try:
-        #s.connect((address, port), timeout=10000)
         s.connect((address, port))
-        return '<h1>{} connection: ok</h1>'.format(name)
+        return '{} connection: ok'.format(name)
     except:
-        return '<h1>{} connection: failed</h1>'.format(name)
+        return '{} connection: failed'.format(name)
     finally:
         s.close()
 
@@ -36,11 +29,10 @@ def connection_test(name, address, port):
 @app.route('/')
 def test():
     result = ""
-    result += connection_test("postgres", DATABASE_HOST, 5432)
-    result += connection_test("redis", REDIS_HOST, 6379)
+    result += connection_test("postgres", get_env_variable("DATABASE_HOST"), 5432)
+    result += connection_test("redis", get_env_variable("REDIS_HOST"), 6379)
     result += connection_test("app_be", "127.0.0.1", 9090)
-    #result += connection_test("Fail check", "10.255.255.1", 9999)
-    result += "DATABASE_PASSWORD: {}".format(DATABASE_PASSWORD)
+    result += "DATABASE_PASSWORD: {}".format(get_env_variable("DATABASE_PASSWORD"))
     return result
 
 
