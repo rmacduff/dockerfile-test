7d819c95d16dbd2647fd56a17dadf41e365cfa8e
Ross Macduff <ross.macduff@slalom.com>
2018-05-25 15:01:06 -0400

    format output

--- app.py
+++ app.py
@@ -29,10 +29,10 @@ def connection_test(name, address, port):
 @app.route('/')
 def test():
     result = ""
-    result += connection_test("postgres", get_env_variable("DATABASE_HOST"), 5432)
-    result += connection_test("redis", get_env_variable("REDIS_HOST"), 6379)
-    result += connection_test("app_be", "127.0.0.1", 9090)
-    result += "DATABASE_PASSWORD: {}".format(get_env_variable("DATABASE_PASSWORD"))
+    result += "{}<br>".format(connection_test("postgres", get_env_variable("DATABASE_HOST"), 5432))
+    result += "{}<br>".format(connection_test("redis", get_env_variable("REDIS_HOST"), 6379))
+    result += "{}<br>".format(connection_test("app_be", "127.0.0.1", 9090))
+    #result += "{}<br>".format("DATABASE_PASSWORD: {}".format(get_env_variable("DATABASE_PASSWORD")))
     return result
 
 
